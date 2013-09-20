89deb819aac6946227dc9b9d833be22ff227aeb3
franklinjagan <franklinjagan@arrivusystems.com>
2013-09-20 17:19:30 +0530

    new changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,68 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+ WebFontConfig = {
+    google: { families: [ 'Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+
+
+      jQuery(document).ready(function($) {
+
+                $('#myCarousel').carousel({
+                    interval: 5000
+                });
+
+                $('#carousel-text').html($('#slide-content-0').html());
+
+                //Handles the carousel thumbnails
+                $('[id^=carousel-selector-]').click( function(){
+                    var id_selector = $(this).attr("id");
+                    var id = id_selector.substr(id_selector.length -1);
+                    var id = parseInt(id);
+                    $('#myCarousel').carousel(id);
+                });
+
+
+                // When the carousel slides, auto update the text
+                $('#myCarousel').on('slid', function (e) {
+                    var id = $('.item.active').data('slide-number');
+                    $('#carousel-text').html($('#slide-content-'+id).html());
+                });
+
+
+            });
+
+         $(window).load(function() {
+             $('#myCarousel').carousel();
+
+             $(".carousel-nav a").click(function(e){
+                 e.preventDefault();
+                 var index = parseInt($(this).attr('data-to'));
+                 $('#myCarousel').carousel(index);
+                 var nav = $('.carousel-nav');
+                 var item = nav.find('a').get(index);
+                 nav.find('a.active').removeClass('active');
+                 $(item).addClass('active');
+             });
+
+             $("#myCarousel").bind('slide', function(e) {
+                 var elements = 3; // change to the number of elements in your nav
+                 var nav = $('.carousel-nav');
+                 var index = $('#myCarousel').find('.item.active').index();
+                 index = (index == elements - 1) ? 0 : index + 1;
+                 var item = nav.find('a').get(index);
+                 nav.find('a.active').removeClass('active');
+                 $(item).addClass('active');
+             });
+         });
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def create
+    @application = Application.find params[:application_code]
+    @errors = ContactUsForm.validate_fields(params, @application)
+    @application.save_contact_us_form(params[:contact_us_form]) if @errors.blank?
+
+    respond_to do |format|
+      format.js #{render :json => {:status => 'success'}.to_json}
+    end
+  end
 end
