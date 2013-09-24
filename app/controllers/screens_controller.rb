fbd2e357eea4b75cd1c45e2a79ac8424e80ec28b
franklinjagan <franklinjagan@arrivusystems.com>
2013-09-24 20:03:36 +0530

    pdf changes

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -4,12 +4,12 @@ class ScreensController < ApplicationController
 	end
 
 	def download_pdf_pgpba
-	    path = "#{Rails.root}/public/pdf/Opportunities in Analytics - Great Lakes.pdf"
+	    path = "#{Rails.root}/public/pdf/Infographic Guide to Careers in Analytics.pdf"
 	    send_data File.read(path),:filename => "Opportunities in Analytics - Great Lakes.pdf",:type => "application/pdf" ,disposition: "attachment"
 	
 	end
 	def download_brchr_pgpba
-    path = "#{Rails.root}/public/pdf/PGPBA - Great Lakes - Brochure.pdf"
+    path = "#{Rails.root}/public/pdf/Great Lakes_PGPBA Brochure.pdf"
     send_data File.read(path),:filename => "PGPBA - Great Lakes - Brochure.pdf",:type => "application/pdf" ,disposition: "attachment"
   end
 end
