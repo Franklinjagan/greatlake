b2baf48ea5fb44f64e02eca415faa053325bbf19
franklinjagan <franklinjagan@arrivusystems.com>
2013-10-29 10:02:28 +0530

    form chages

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,7 +1,7 @@
 class ScreensController < ApplicationController
 	
 	def index
-		@download_file= false
+		  @download_file= false
 		if request.referer.present? && request.referer.include?('businessanalyticsprogram')
 			@download_file = true
 		else
@@ -10,7 +10,7 @@ class ScreensController < ApplicationController
 	end
 
 	def download_pdf_pgpba
-		path = "#{Rails.root}/public/pdf/Infographic Guide to Careers in Analytics.pdf"
+		path = "#{Rails.root}/public/pdf/Business_Analytics_Program_Great_Lakes.pdf"
 		send_data File.read(path),:filename => "Opportunities in Analytics - Great Lakes.pdf",:type => "application/pdf" ,disposition: "attachment"
 	end
 
