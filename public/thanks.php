34f62c7f5a7dedfb948a5bff8b586529c09ce853
franklinjagan <franklinjagan@arrivusystems.com>
2014-03-05 12:06:58 +0530

    landing page changes

---
+++ public/thanks.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Thank You!</title>
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/form.css" rel="stylesheet" />
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/parsley.js" type="text/javascript"></script>
+<script type="text/javascript">
+function MM_goToURL() { //v3.0
+  var i, args=MM_goToURL.arguments; document.MM_returnValue = false;
+  for (i=0; i<(args.length-1); i+=2) eval(args[i]+".location='"+args[i+1]+"'");
+}
+</script>
+
+</head>
+
+<body>
+
+<!-- Google Code for Sign Up Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 975301779;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "Z0RqCK2yjAkQk9mH0QM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/975301779/?label=Z0RqCK2yjAkQk9mH0QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+
+
+
+<div class="wrapper">
+<div class="topbannerareathanks">
+   <p>&nbsp;</p>
+   <div class="headingbig bold mrleft500">
+     
+     <div class="headingbig">
+       <p>Thank You!</p>
+     </div>
+   </div>
+  <div class="smallheading lineheight25 mrleft500" onfocus="MM_goToURL('parent','http://pgpba.greatlakes.edu.in/businessanalyticsprogram/PGPBA_great_lakes_brochure.pdf');return document.MM_returnValue">
+    <p>A weblink for 
+      the session &amp;<br /> 
+      information about date &amp; time <br />
+      will be emailed to you.</p>
+    <p>Please <span class="bold linkstyle blackregular"><a href="Opportunities-in-Analytics-GreatLakes.pdf">click here</a></span> for download                                                                              <br />
+      FREE ebook on careers in analytics. </p>
+  </div>
+</div>
+
+<div class="clear"></div>
+
+<div class="yellowpart aligncenter lineheight25 ptopbottom50 font18"><span class="grey-button"><a href="http://pgpba.greatlakes.edu.in/">Click here</a></span> to learn more about PGP-BA course. <br />
+  <br />
+  Visit our <span class="grey-button"><a href="http://www.greatlakes.edu.in/" target="_blank">website</a></span> to learn more about Great Lakes &amp; the other courses we offer. </div>
+
+<div class="clear"></div>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div class="clear"></div>
+<div class="line mtbottom20">&nbsp;</div>
+<div class="footer"> <img src="images/logo-footer.jpg" width="209" height="76" class="logo" />
+  <!--  <div class="devider floatleft">&nbsp;</div>-->
+  <div class="box floatleft">Great Lakes, one of India's premier business schools, has been ranked within the top ten in the country by Outlook, Business Today, Careers 360 and CNBC-TV18.<br />
+  </div>
+  <!--<div class="devider floatleft">&nbsp;</div>-->
+  <div class="box floatleft"> For more information:<br/>
+    <div class="icontextarea lineheight25 mrtop14"><img src="images/footer-icons-call.jpg" width="23" height="25" />Call 08750648238</div>
+    <div class="icontextarea lineheight25 mrtop5"><img src="images/footer-icons-mail.jpg" width="23" height="25" />Email <a href="mailto:pgpba@greatlakes.edu.in">pgpba@greatlakes.edu.in</a></div>
+    <br />
+  </div>
+</div>
+</div>
+
+
+
+</div>
+
+<!-- Google Code for Remarketing Tag -->
+<!--------------------------------------------------
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 975301779;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/975301779/?value=0&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+</body>
+</html>
